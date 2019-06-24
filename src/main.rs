3c0f861fd12db0c22db0dd2da265d1a51c043cf8
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2019-06-24 22:31:55 +0800

    Move read file logic to own fn

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::fs;
 use std::process;
 
+use std::error::Error;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -13,9 +15,10 @@ fn main() {
     println!("Searching for '{}'", config.query);
     println!("In file '{}'", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents)
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -34,4 +37,11 @@ impl Config {
 
         Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    
+    println!("With text:\n{}", contents);
+    Ok(())
 }
